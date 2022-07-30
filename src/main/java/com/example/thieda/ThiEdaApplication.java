4a924fdd7f565292830c31174e9b4e4068d999fe
Viet Hoang <viethoangd33@gmail.com>
2022-07-30 14:53:51 +0700

    Initial commit

---
+++ src/main/java/com/example/thieda/ThiEdaApplication.java
@@ -0,0 +1,13 @@
+package com.example.thieda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThiEdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThiEdaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thieda/ThiEdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thieda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThiEdaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
